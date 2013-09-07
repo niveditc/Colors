0756b06623ca235f213d81be8f9f31299b193da5
Isaac Lim <isaaclimdc@gmail.com>
2013-09-07 01:15:34 -0400

    Added iOS project

---
+++ Colors/Colors/COLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  COLAppDelegate.h
+//  Colors
+//
+//  Created by Isaac Lim on 9/7/13.
+//  Copyright (c) 2013 isaacl.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface COLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Colors/Colors/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Colors
+//
+//  Created by Isaac Lim on 9/7/13.
+//  Copyright (c) 2013 isaacl.net. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "COLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([COLAppDelegate class]));
+    }
+}
