7583df59aa4f051f3848812101dd5898653ded2c
raemadeline <raemadeline@gmail.com>
2013-09-06 22:15:44 -0400

    Clear Screen

--- js/ui.js
+++ js/ui.js
@@ -3,3 +3,12 @@ var ctx = canvas.getContext("2d");
 ctx.font = "60px Arial";
 ctx.textAlign = "center";
 ctx.fillText("Hello World!", 200, 200);
+
+/* G: Graph */
+function renderGraph(G) {
+	ctx.clearRect(0, 0, 700, 700);
+
+	//render graph
+}
+
+//touch / click event
