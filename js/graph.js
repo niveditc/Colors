47dc02736448ceb09c4bd78dffb5391ce197c66b
Nivedita Chopra <niveditc@andrew.cmu.edu>
2013-09-06 21:17:36 -0400

    Created the basic template for the graph and vertex

--- js/graph.js
+++ js/graph.js
@@ -1,3 +1,12 @@
-Object Graph {
-    
+function Vertex{
+    this.neighbors = [];
+    this.color = 0;
+}
+
+function Graph(numVertices) {
+    this.numVertices = numVertices;
+    this.vertices = new Array(numVertices);
+    for(var i = 0; i < num_vertices; i++){
+        this.vertices[i] = new Vertex();
+    }
 }
