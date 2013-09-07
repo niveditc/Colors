14a338180221fe6bbbad2a954eb703fd13eb7516
raemadeline <raemadeline@gmail.com>
2013-09-06 22:43:47 -0400

    Draw Graph

--- js/ui.js
+++ js/ui.js
@@ -6,6 +6,12 @@ ctx.fillText("Hello World!", 200, 200);
 
 /* grey (default), red, blue, green, orange, purple */
 var colors = ["#D0D0D0", "#FF6347", "#40E0D0", "#9ACD32", "#FFA500", "#6A5ACD"];
+var radius = 10;
+var line = 3;
+
+function circle(ctx, cx, cy, radius) {
+    ctx.arc(cx, cy, radius, 0, 2*Math.PI, true);
+}
 
 /* G: Graph */
 function renderFirstGraph(G) {
@@ -21,6 +27,8 @@ function renderFirstGraph(G) {
 
 	//draw graph edges
 	for(var i = 0; i < G.numVertices; i++) {
+		ctx.lineWidth = line;
+
 		var currVertex = G.vertices[i];
 		var numNeighbors = currVertex.neighbors.length;
 		var p1 = currVertex.loc;
@@ -34,13 +42,23 @@ function renderFirstGraph(G) {
 		});
 	}
 
-	//draw vertices in default color
-
-	//randomly generate graph
+	updateColors(G);
 }
 
-function renderNewGraph(G) {
-	//take in vertex locations and colors
+/* G: Graph */
+function updateColors(G) {
+
+	for(var i = 0; i < G.numVertices; i++) {
+		var V = G.numVertices[i];
+
+		ctx.fillStyle = V.color;
+
+		ctx.beginPath();
+		var cx = V.loc.x;
+		var cy = V.loc.y;
+		circle(ctx, cx, cy, radius);
+		ctx.fill();
+	}
 }
 
 /* i: int (num vertices) // returns: array of points */
